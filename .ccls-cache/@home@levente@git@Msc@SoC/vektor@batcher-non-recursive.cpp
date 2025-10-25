d81e6a64a0ba21846f731238dad33d45fd7c5242
leventenyiri3 <nyiri.levente3@gmail.com>
2025-10-25 15:06:06 +0200

    loopos verzio

--- .ccls-cache/@home@levente@git@Msc@SoC/vektor@batcher-non-recursive.cpp
+++ .ccls-cache/@home@levente@git@Msc@SoC/vektor@batcher-non-recursive.cpp
@@ -15,10 +15,8 @@ void compare(uint8_t* array, int i, int j)
 
 void batcher_non_recursive(uint8_t* array)
 {
-  uint8_t a,b,c,d;
-  a = 0;
-  b = a+1;
-  c = a+2;
+  uint8_t d, merge_index;
+  merge_index = 0;
   d = 0;
 
 
@@ -26,113 +24,40 @@ void batcher_non_recursive(uint8_t* array)
   for(int i=0; i<32; i++)
       inner_array[i] = *(array + i);
   //ide nyilván ismétlés helyett logikusabb lenne ciklussal, de a párhuzamosításnál így talán jobban átlátom
+  for(int base=0; base<32; base+=4)
+  {
+
+  compare(inner_array, base, base+1); // ez a kettő még mehet párhuzamosan akár
+  compare(inner_array, base+2, base+3); //
+  compare(inner_array, base, base+2);
+  compare(inner_array, base+1, base+3);
+  compare(inner_array, base+1, base+2);
+
+  printf("\n");
+  printf("a = %d", base);
+  printf("\n");
+  for(int i=0; i<32; i++)
+    printf("%d ", inner_array[i]);
+
+    if(base%8 == 0 && base != 0)
+    {
+      compare(inner_array, merge_index, merge_index+4);
+      compare(inner_array, merge_index+2, merge_index+6);
+      compare(inner_array, merge_index+2, merge_index+4);
+      compare(inner_array, merge_index+1, merge_index+5);
+      compare(inner_array, merge_index+3, merge_index+7);
+      compare(inner_array, merge_index+3, merge_index+5);
+
+      compare(inner_array, merge_index+1, merge_index+2);
+      compare(inner_array, merge_index+3, merge_index+4);
+      compare(inner_array, merge_index+5, merge_index+6);
+    }
+
   
-  compare(inner_array, a, a+1); // ez a kettő még mehet párhuzamosan akár
-  compare(inner_array, a+2, a+3); //
-  compare(inner_array, a, a+2);
-  compare(inner_array, a+1, a+3);
-  compare(inner_array, a+1, a+2);
 
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
-  a = 4;
+  }
 
-  compare(inner_array, a, a+1); 
-  compare(inner_array, a+2, a+3); 
-  compare(inner_array, a, a+2);
-  compare(inner_array, a+1, a+3);
-  compare(inner_array, a+1, a+2);
 
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
-
-/* Ez a 8-al megnövelt mehet párhuzamosan az előzővel */
-  a = 8;
-
-  compare(inner_array, a, a+1); 
-  compare(inner_array, a+2, a+3); 
-  compare(inner_array, a, a+2);
-  compare(inner_array, a+1, a+3);
-  compare(inner_array, a+1, a+2);
-
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
-  a = 12;
-
-  compare(inner_array, a, a+1);
-  compare(inner_array, c, c+1);
-  compare(inner_array, a, a+2);
-  compare(inner_array, b, b+2);
-  compare(inner_array, b, b+1);
-
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
-//másik fél
-
-  a = 16;
-
-  compare(inner_array, a, a+1);
-  compare(inner_array, c, c+1);
-  compare(inner_array, a, a+2);
-  compare(inner_array, b, b+2);
-  compare(inner_array, b, b+1);
-
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
-  a = 20;
-
-  compare(inner_array, a, a+1);
-  compare(inner_array, c, c+1);
-  compare(inner_array, a, a+2);
-  compare(inner_array, b, b+2);
-  compare(inner_array, b, b+1);
-  
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
-  a = 24;
-
-  compare(inner_array, a, a+1);
-  compare(inner_array, c, c+1);
-  compare(inner_array, a, a+2);
-  compare(inner_array, b, b+2);
-  compare(inner_array, b, b+1);
-
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
-  a = 28;
-
-  compare(inner_array, a, a+1);
-  compare(inner_array, c, c+1);
-  compare(inner_array, a, a+2);
-  compare(inner_array, b, b+2);
-  compare(inner_array, b, b+1);
-  
-  printf("\n");
-  printf("a = %d", a);
-  printf("\n");
-  for(int i=0; i<32; i++)
-    printf("%d ", inner_array[i]);
 // ez előtt lévők mehetnek full párhuzamosan, itt be kell várniuk
 /* Nagyobb mergelés itt kezdődik*/
 
